ac83d096d7dde21434c8e36a3a728ecf3834b743
cjrieck <cjrieck123@gmail.com>
2014-11-10 15:17:37 -0500

    set up basic root tableview so you can choose the demo you'd like to see

--- App/iOS-Custom-UIs/View Controllers/CURootMainMenuViewController.m
+++ App/iOS-Custom-UIs/View Controllers/CURootMainMenuViewController.m
@@ -8,16 +8,37 @@
 
 #import "CURootMainMenuViewController.h"
 
-@interface CURootMainMenuViewController ()
+static NSString * const kCURootViewControllerTitle = @"Demos";
+
+@interface CURootMainMenuViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *rootTableView;
+@property (strong, nonatomic) NSArray *demoTitles;
 
 @end
 
 @implementation CURootMainMenuViewController
 
+- (instancetype)init {
+    self = [super init];
+    if ( self ) {
+        _demoTitles = @[@"Circular CollectionView"];
+        
+        self.title = kCURootViewControllerTitle;
+    }
+    return self;
+}
+
 - (void)loadView
 {
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    UITableView *rootTableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    rootTableView.delegate = self;
+    rootTableView.dataSource = self;
+    [self.view addSubview:rootTableView];
+    _rootTableView = rootTableView;
 }
 
 - (void)viewDidLoad {
@@ -28,6 +49,35 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView delegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    //TODO: Transistion to apropriate demo project
+}
+
+#pragma mark - UITableView datasource methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.demoTitles.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = [self.demoTitles objectAtIndex:indexPath.row];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
 }
 
 @end
